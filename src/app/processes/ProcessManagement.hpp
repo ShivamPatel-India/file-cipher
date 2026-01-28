2bf6f18364ed5453d26d30b4a5e94264bd6b7edb
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-27 19:22:33 -0800

    Add multiprocessing with shared memory for encrypt/decrypt

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -4,14 +4,36 @@
 #include "Task.hpp"
 #include <memory>
 #include <queue>
+#include <semaphore.h>
 
 class ProcessManagement {
+    sem_t* itemsSemaphore; // space occupied
+    sem_t* emptySlotsSemaphore; // space available
+
+
     public:
         ProcessManagement();
+        ~ProcessManagement(); 
         bool submitToQueue(std::unique_ptr<Task> task);
         void executeTasks();
     private:
-        std::queue<std::unique_ptr<Task>> taskQueue;
+        struct SharedMemory {
+            std::atomic<int> size; 
+            char tasks[1000][256]; // queue that can accomodate 1000 tasks
+            int front;
+            int rear;
+
+            void printSharedMemory() {
+                std::cout<< size << std::endl;
+                std::cout<<front<<std::endl;
+                std::cout<<rear<<std::endl;
+            }
+        };
+
+        SharedMemory* sharedMem;
+        int shmFd;
+        const char* SHM_NAME = "/my_queue";
+        std::mutex queueLock;
 };
 
 #endif
